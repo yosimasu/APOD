f2e91a7f8d9c51ac9eaf9bda72de6e6dbbaa4e35
Masu Lin <yosimasu@gmail.com>
2018-04-12 11:38:43 +0800

    component(Astronomy)

---
+++ app/components/Astronomy/component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Astronomy extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Astronomy</Text>
+            </View>
+        );
+    }
+}
+
+export default Astronomy;
